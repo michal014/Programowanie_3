38637b71ba69ea698753993eb3108d34bc355dfd
michal014 <39431559+michal014@users.noreply.github.com>
2022-10-21 11:48:09 +0200

    List2/zad2_finished

---
+++ Lista_2/Kolko_krzyzyk/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kolko_krzyzyk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
