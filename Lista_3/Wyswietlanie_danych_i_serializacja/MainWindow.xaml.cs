ec656c9ea8541724f39953d139187cec598e0504
michal014 <39431559+michal014@users.noreply.github.com>
2022-12-02 11:54:37 +0100

    L3/Z1 Half Way. Need to implement save and load

--- Lista_3/Wyswietlanie_danych_i_serializacja/MainWindow.xaml.cs
+++ Lista_3/Wyswietlanie_danych_i_serializacja/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace Wyswietlanie_danych_i_serializacja
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> personsList = new List<Person>();
         public MainWindow()
         {
             InitializeComponent();
+            dataGridPerson.ItemsSource = personsList;
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            WindowAddPerson window1 = new WindowAddPerson();
+            Person person= new Person();
+            window1.DataContext= person;
+            window1.ShowDialog();
+            if (window1.shouldBeSaved)
+            {
+                personsList.Add(person);
+                dataGridPerson.Items.Refresh();
+            }
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+            if (personsList.Count > 0)
+            {
+                WindowAddPerson window2 = new WindowAddPerson();
+                Person person2 = new Person((Person)dataGridPerson.SelectedItem);
+                window2.DataContext = person2;
+                window2.ShowDialog();
+                if (window2.shouldBeSaved)
+                {
+                    personsList[personsList.IndexOf((Person)dataGridPerson.SelectedItem)] = person2;
+                    dataGridPerson.Items.Refresh();
+                }
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (personsList.Count > 0)
+            {
+                personsList.RemoveAt(personsList.IndexOf((Person)dataGridPerson.SelectedItem));
+                dataGridPerson.Items.Refresh();
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Lista_3/Wyswietlanie_danych_i_serializacja/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyswietlanie_danych_i_serializacja
+{
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public string pesel { get; set; }
+        
+
+        public Person()
+        {
+            this.firstName = string.Empty;
+            this.lastName = string.Empty;
+            this.gender = string.Empty;
+            this.pesel = string.Empty;
+        }
+
+        public Person(string firstName, string lastName,string plec, string pesel)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = plec;
+            this.pesel = pesel;
+        }
+
+        public Person(Person person)
+        {
+            this.firstName = person.firstName;
+            this.lastName = person.lastName;
+            this.pesel = person.pesel;
+            this.gender = person.gender;
+        }
+    }
+}
